7e9a408e9c1d1f9d579d4d29175c180b1b716419
Duy Toan <toanpmo123@gmail.com>
2021-11-07 15:50:35 +0700

    update

--- FE/chatrealtime/src/App.js
+++ FE/chatrealtime/src/App.js
@@ -1,18 +1,18 @@
 import 'react-bootstrap'
-import './App.css';
-import Lobby from './components/Lobby';
-import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
-import { useState } from 'react';
+import './App.css'
+import Lobby from './components/Lobby'
+import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr'
+import { useState } from 'react'
 
 const App = () => {
-  const [connection, setConnection] = useState();
+  const [ connection, setConnection] = useState()
 
   const joinRoom = async(user, room) => {
     try {
       const connection = new HubConnectionBuilder()
       .withUrl("https://localhost:44315/chat")
-      .configureLogging(LogLevel.Information)
-      .build();
+      .configureLogging(LogLevel.Infomation)
+      .build()
 
 
       connection.on("ReceiveMessage", (user, message) => {
