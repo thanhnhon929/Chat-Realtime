a4ac7c59a1b86cdaa90e9da4e48d01bced7f795d
Duy Toan <toanpmo123@gmail.com>
2021-11-07 11:26:55 +0700

    up layout BE

---
+++ BE/Chatrealtime/Chatrealtime/Hubs/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chatrealtime.Hubs
+{
+    public class ChatHub : Hub
+    {
+        //ChatBot
+        private readonly string _botUser;
+
+        public ChatHub()
+        {
+            _botUser = "Mychat Bot";
+        }
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+
+            await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _botUser,
+                $"{userConnection.User} has joined {userConnection.Room}");
+        }
+    }
+}
